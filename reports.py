35378308d54ca83e22438b542449318b98ade0c7
Barbara Bochińska <bachna22@gmail.com>
2018-06-28 22:26:46 +0200

    added functions: count_games , decide

--- reports.py
+++ reports.py
@@ -1,2 +1,28 @@
+def count_games(file_name):
+    file = open(file_name)
+    how_many = len(file.readlines())
+    file.close()
+    return how_many
+    
 
-# Report functions
+def decide(file_name, year):
+    result = False
+    year = str(year)
+    file = open(file_name)
+    if year in file.read():
+        result = True
+    else:
+        result = False
+    file.close()
+    return result
+    
+def get_latest(file_name):
+    
+
+
+    
+def count_by_genre(file_name, genre):
+    pass
+    
+def get_line_number_by_title(file_name, title):
+    pass
