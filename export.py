26148f6a895461d79596e3b4037491b927d842c4
Barbara Bochińska <bachna22@gmail.com>
2018-06-29 22:32:55 +0200

    added new file: export.txt

--- export.py
+++ export.py
@@ -1,2 +1,11 @@
+import reports
 
-# Export functions
+def export(answer):
+    filename = open("export.txt", "w")
+    filename.write(str(reports.count_games(answer)) + "\n")
+    filename.write(str(reports.decide(answer, 1998)) + "\n")
+    filename.write(str(reports.get_latest(answer)) + "\n")
+    filename.write(str(reports.count_by_genre(answer, "RPG")) + "\n")
+    filename.write(str(reports.get_line_number_by_title(answer, "StarCraft")) + "\n")
+
+export("game_stat.txt")
