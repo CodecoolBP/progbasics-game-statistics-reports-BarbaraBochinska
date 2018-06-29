dac544befa6da69386e3496dc0e100e9d138f178
Barbara Bochińska <bachna22@gmail.com>
2018-06-29 23:53:56 +0200

    added functions: sum_sold and get selling_avg

--- part2/reports.py
+++ part2/reports.py
@@ -11,3 +11,25 @@ def get_most_played(file_name):
     game_title = titles[biggest_sale]
     return game_title
 
+def sum_sold(file_name):
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    sold_games = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split("\t")
+        sold_games.append(float(lines[i][1]))
+    total_sum = sum(sold_games)
+    return total_sum
+
+def get_selling_avg(file_name):
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    sold_games = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split("\t")
+        sold_games.append(float(lines[i][1]))
+    total_sum = sum(sold_games)
+    avg_sale = total_sum / len(lines)
+    return avg_sale
+
+
