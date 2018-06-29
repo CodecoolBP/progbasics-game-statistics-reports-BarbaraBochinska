b6cfa18765d8b3faf6a413873fc5763596676598
Barbara Bochińska <bachna22@gmail.com>
2018-06-29 23:24:33 +0200

    added function: get_most_played in reports module

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,13 @@
+def get_most_played(file_name):
+    with open (file_name, "r") as file:
+        lines = file.readlines()
+    sale = []
+    titles = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split("\t")
+        sale.append(float(lines[i][1]))   
+        titles.append(lines[i][0])
+    biggest_sale = sale.index(max(sale))
+    game_title = titles[biggest_sale]
+    return game_title
 
-# Report functions
