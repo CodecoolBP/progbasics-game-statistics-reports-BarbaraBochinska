5376b851b3c5aec391bc52a1fb8b1d28d9531209
Barbara Bochińska <bachna22@gmail.com>
2018-06-30 23:23:37 +0200

    fixed wrong function call

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,12 @@
+import reports
 
-# Export functions
+def export(file):
+    filename = open("export.txt", "w")
+    filename.write(str(reports.get_most_played("game_stat.txt")) + "\n")
+    filename.write(str(reports.sum_sold("game_stat.txt")) + "\n")
+    filename.write(str(reports.get_selling_avg("game_stat.txt")) + "\n")
+    filename.write(str(reports.count_longest_title("game_stat.txt")) + "\n")
+    filename.write(str(reports.get_date_avg("game_stat.txt")) + "\n")
+
+export("game_stat.txt")
+
