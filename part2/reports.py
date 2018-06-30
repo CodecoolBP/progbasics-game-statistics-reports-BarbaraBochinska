7290d7fc09738af3d8335a067c2539683f629ce0
Barbara Bochińska <bachna22@gmail.com>
2018-06-30 17:24:49 +0200

    added get date avg function

--- part2/reports.py
+++ part2/reports.py
@@ -32,4 +32,26 @@ def get_selling_avg(file_name):
     avg_sale = total_sum / len(lines)
     return avg_sale
 
+def count_longest_title(file_name):
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    titles = []
+    length = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split("\t")
+        titles.append(lines[i][0])
+        length.append(len(titles[i]))
+    longest_length = max(length)
+    return longest_length
 
+def get_date_avg(file_name):
+    with open(file_name, "r") as file:
+        lines = file.readlines()
+    release_dates = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split("\t")
+        release_dates.append(int(lines[i][2]))
+    sum_dates = sum(release_dates)
+    avg_release_float = sum_dates / len(lines)
+    avg_release= int(round(avg_release_float))
+    return avg_release
